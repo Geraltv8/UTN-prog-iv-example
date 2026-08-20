b64f92db8b858833c8f64b7a851d226909a1d6b1
Geraltv8 <geraquesearock@gmail.com>
2026-08-20 16:21:31 -0300

    se instalo express, nodemon, morgan

--- app.js
+++ app.js
@@ -1,43 +1,34 @@
-import numeroPi, { sumar, restar } from './math.js';
-import asincrona from './asincronia.js';
-import { Admin } from './admin.js';
-import { Usuario } from './usuario.js';
-import { Producto } from './producto.js';
+import express from 'express';
+import morgan from 'morgan';
 
-console.log("---- Iniciando Sistema -----");
+const app = express();
 
-const productoNuevo = new Producto("Teclado", 300.3333);
+//Middlewares globales
+app.use(morgan('dev'));
+app.use(express.json());
 
-console.log(productoNuevo.precio);
-
-productoNuevo.precio = 1231.323223;
-console.log(productoNuevo.precio);
-productoNuevo.precio = -200;
-
-/*const superAdmin = new Admin("admin@gmail.com", 1234567, 4);
-const usuario1 = new Usuario("admin@gmail.com", "1234567");
-
-superAdmin.login("1234567");
-superAdmin.login("1111111");
-superAdmin.borrarBaseDeDatos();
-
-console.log(`El mail es ${superAdmin.email}`);
-console.log(`la contraseña es ${superAdmin.password}`);
-
-console.log(`la contraseña es ${usuario1.password}`); */
-
-//asincrona();
-
-//console.log("suma:", sumar(10, 5));
-//console.log("el valor de PI es:", numeroPi);
+const PORT = 3000;
 
 
+app.post('/registro', (req, res) =>{
+    const cuerpoPeticion = req.body;
 
+    res.status(201).json({
+        mensaje: "Recurso Creado con existo",
+        datosRecibidos: cuerpoPeticion
+    });
+});
 
-//CREAR UN ARCHIVO usuario.js y una clase usuario 
-// que reciba email y password (la pass debe ser privada)
+app.get('/perfil/:id', (req, res) => {
+    const idUsuario = req.params.id;
+    res.send(`Hola Mundo! y hola ${idUsuario}`);
+});
 
-//CREAR UN ARCHIVO Admin.js que importe usuario y crear una clase Admin que herede de Usuario
-//y un metodo, que se llame borrarBaseDeDatos()
+app.get('/buscar', (req, res) => {
+    const categoria = req.query.categoria;
+    res.send(`Hola Mundo 2! estas buscando: ${categoria}`);
+});
 
-//app.js crear un objeto admin y probar el metodo
+app.listen(PORT, () => {
+    console.log(`Servidor Express listo en http://localhost:${PORT}`);
+});
