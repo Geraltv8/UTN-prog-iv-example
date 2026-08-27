9bc0c521e310fd3d442238fd2243107da8cafb80
Geraltv8 <geraquesearock@gmail.com>
2026-08-27 17:03:57 -0300

    modificado routes de proveedores y agregado mock json

--- routes/proveedores.routes.js
+++ routes/proveedores.routes.js
@@ -1,20 +1,14 @@
 import { Router } from 'express';
 
+import {
+    crearProveedor,
+    obtenerProveedores
+} from '../controllers/proveedor.controller.js'
+
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({mensaje: "Lista de todos los proveedores"});
-});
-
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    res.json({ mensaje: "Detelles del proveedor ID:" + id});
-});
-
-router.post('/', (req, res) => {
-    const nuevoProveedor = req.body;
-    res.status(201).json({ mensaje: "Proveedor registrado", data: nuevoProveedor});
-});
+router.post('/', crearProveedor);
+router.get('/', obtenerProveedores);
 
 export default router;
 
